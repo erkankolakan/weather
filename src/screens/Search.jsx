441cd54fba026a3d0c373bb1acd246c290a31ea3
erkankolakan <erkankolakan@gmail.com>
2024-04-03 22:14:13 +0300

    svgler react a  uygun olacak şekilde güncellendi

---
+++ src/screens/Search.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from '../components/Header'
+
+import Background from '../../assets/Background'
+
+const Search = () => {
+  return (
+    <View className=" flex-1">
+      {/* <Header/> */}
+      <Background />
+    </View>
+  )
+}
+
+export default Search
