ec8499bbe0ced44d484afa3aba285c853d18307d
erkankolakan <erkankolakan@gmail.com>
2024-04-04 13:05:45 +0300

    nunito fontları yüklendi ve kuruldu react için font paketi kullanıldı

--- App.js
+++ App.js
@@ -2,14 +2,25 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider } from "native-base";
 import { createStackNavigator } from "@react-navigation/stack";
+import { useFonts } from "expo-font";
 
 // screens
-import { Home } from "./src/screens/index";
+import { Home,Search } from "./src/screens/index";
 
 
 export default function App() {
-
   const Stack = createStackNavigator();
+  
+  
+  const [fontsLoaded, fontError] = useFonts({
+    nunito: require("./assets/fonts/Nunito-Regular.ttf"),
+    nunitoExtraBold: require("./assets/fonts/Nunito-ExtraBold.ttf"),
+    nunitoBold: require("./assets/fonts/Nunito-Bold.ttf"),
+    nunitoBlack: require("./assets/fonts/Nunito-Black.ttf"),
+    nunitoSemiBold: require("./assets/fonts/Nunito-SemiBold.ttf"),
+  });
+  
+  
   return (
     <NativeBaseProvider>
       <NavigationContainer>
@@ -24,6 +35,8 @@ export default function App() {
             }),
           }}
         >
+          <Stack.Screen name="Search" component={Search} />
+          
           <Stack.Screen name="Home" component={Home} />
         </Stack.Navigator>
       </NavigationContainer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+      "nunito":'./assets/fonts/Nunito-Regular.ttf',
+      "nunitoExtraBold":'./assets/fonts/Nunito-ExtraBold.ttf',
+      "nunitoBold":'./assets/fonts/Nunito-Bold.ttf',
+      "nunitoBlack":'./assets/fonts/Nunito-Black.ttf',
+      "nunitoSemiBold":'./assets/fontsNunito-SemiBold.ttf',
+      
+      extend: {
+        colors:{
+          "white":"#DB4221",
+          "black":"#000000",
+        },
+      },
+
+
+    fontFamily: {
+      "nunito": ['nunito'],
+      "nunitoExtraBold": ['nunitoExtraBold'],
+      "nunitoBold": ['nunitoBold'],
+      "nunitoBlack": ['nunitoBlack'],
+      "nunitoSemiBold": ['nunitoSemiBold'],
+    },
+
+
+    
   },
   plugins: [],
 }
