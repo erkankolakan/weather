ec3afccf17236fa323c56db75a5b357296944a84
erkankolakan <erkankolakan@gmail.com>
2024-04-19 21:55:15 +0300

    resimler

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,17 +2,16 @@ import { View, Text, TouchableOpacity } from "react-native";
 import React, { useState, useEffect } from "react";
 import { Box, Input, Spinner, Heading } from "native-base";
 import { useNavigation } from "@react-navigation/native";
-import { selectMatchingCities } from "../utils/cityFormatters"; 
-import { getWeatherData } from "../utils/WeatherOperations"; 
+import { selectMatchingCities } from "../utils/cityFormatters";
+import { getWeatherData } from "../utils/WeatherOperations";
 import axios from "axios";
-
 
 const SearchBar = () => {
   const [text, onChangeText] = useState(""); // Boş bir string olarak başlatın
   const [cityList, setCityList] = useState([]); // Şehir listesi için state
   const [loading, setLoading] = useState(false); // Yükleme durumu için state
   const navigation = useNavigation();
-  
+
   useEffect(() => {
     const fetchCities = async () => {
       setLoading(true);
@@ -24,33 +23,28 @@ const SearchBar = () => {
       }
       setLoading(false);
     };
-  
     fetchCities();
   }, [text]);
-  
 
   const handleCityPress = async (city) => {
     const API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${city.cityName}&limit=5&appid=${process.env.API_KEY}`;
     try {
-        const response = await axios.get(API_URL);
-        const { lat, lon } = response.data[0];
-        const weatherData = await getWeatherData(lat, lon);
-        navigation.navigate("Home",
-          {
-            city: city,
-            weatherData: weatherData,
-          }
-      );
+      const response = await axios.get(API_URL);
+      const { lat, lon } = response.data[0];
+      const weatherData = await getWeatherData(lat, lon);
+      navigation.navigate("Home", {
+        city: city,
+        weatherData: weatherData,
+      });
+      onChangeText("");
     } catch (error) {
-        console.error('Error fetching city data:', error);
+      console.error("Error fetching city data:", error);
     }
-};
-
-
+  };
 
   return (
     <View className="h-screen absolute w-full gap-y-4  justify-center items-center ">
-      <View>
+      <View className="px-1">
         <Text className="text-3xl font-nunito text-white text-center ">
           Welcome to <Text className="text-blueText">TypeWeather</Text>
         </Text>
@@ -85,9 +79,11 @@ const SearchBar = () => {
             className="bg-searchBg h-14  justify-center pl-2 border-black border-b-[1px] "
             activeOpacity={0.8}
           >
-            <Text className="text-white font-nunitoSemiBold">
-              {city.cityName} - {city.country}
-            </Text>
+            {
+              <Text className="text-white font-nunitoSemiBold">
+                {city.cityName} - {city.country}
+              </Text>
+            }
           </TouchableOpacity>
         ))}
       </View>
